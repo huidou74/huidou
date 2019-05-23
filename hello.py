2c8271a923fb7bcbcb299831599ed5f12e2d8a2b
H.c <liangyonghui123@qq.com>
2019-05-23 17:52:30 +0800

    V 0.5

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@
 #-*- coding:utf8 -*-
 #BY:  H.c
 print ('HELLO !')
+print ('HELLO !')
 print ('test to webhook!')
 print ('test tag and requests messages')
 import requests  # 发送请求，然后获取相应内容
 url= 'https://api.github.com/repos/huidou74/huidou/tags'
 r = requests.get(url).json()
 print (r,type(r))  # list
+# test to tags API
