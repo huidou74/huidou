10dde9902cfa6eac6c92ae012025355d6d0695f3
H.c <liangyonghui123@qq.com>
2019-05-23 17:08:05 +0800

    test8 to tags and requests

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 #-*- coding:utf8 -*-
 #BY:  H.c
-print ('HELLO !')
-print ('test to webhook!')
-print ('test to webhook!')
-print ('test 7')
+# print ('HELLO !')
+# print ('test to webhook!')
+# print ('test to webhook!')
+# print ('test 7')
+import requests  # 发送请求，然后获取相应内容
+r = requests.get('https://api.github.com/repos/Codertocat/Hello-World/tags').json()
+print (r,type(r))  # list
