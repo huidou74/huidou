abc511ee73c498cd93ac8121b0c1edc4cd11d586
H.c <liangyonghui123@qq.com>
2019-06-06 14:31:25 +0800

    connet to mysql OK

--- hello.py
+++ hello.py
@@ -5,11 +5,11 @@ print ('HELLO !')
 print ('test to webhook!')
 print ('test tag and requests messages')
 print ('test tag and requests messages')
-print ('test tag and requests messages')
-print ('test tag and requests messages')
 import requests  # 发送请求，然后获取相应内容
 url= 'https://api.github.com/repos/huidou74/huidou/tags'
 r = requests.get(url).json()
 print (r,type(r))  # list
 print ('webhook test OK! ')
-print ('V 0.6.1')
+print ('django connet to mysql OK')
+print ('django connet to mysql OK')
+a='v0.6.5'
